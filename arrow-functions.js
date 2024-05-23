3783b23a09f5014209696b4181f6a133af11ab40
RNXVIII <148905439+RNXVIII@users.noreply.github.com>
2024-05-23 15:20:54 +0000

    end lesson

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,43 @@
 
 // Vanilla JavaScript Function
 
+// function addTwoNumbers(a, b){
+
+//     return a+b;
+// }
+
+// let sum = addTwoNumbers(5, 3);
+// console.logz(sum);
+
 // Arrow Function With Parameters
+
+const addTwoNumbers = (a, b) =>{
+
+    return a+b;
+}
+
+let sum = addTwoNumbers(5, 3);
+console.logz(sum);
 
 // Single Line Arrow Function With Parameters
 
-// Implicit Returns
+const addTwoNumbers2 = (a,b) => a+b;
+// const addTwoNumbers2 = (a,b) => (a+b); also valid
+let sum2 = addTwoNumbers2(6,4);
+console.log(sum2);
 
+// Implicit Returns
+const saySomething = message =>  console.log(message);
+saySomething("hello!!!");
+
+const sayHello = () => console.log('hello');
+sayHello()
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+       this is multiple lines
+    </p>`
+)
+
+console.log(returnMultipleLines());
